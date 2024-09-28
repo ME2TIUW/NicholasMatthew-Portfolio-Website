29a4df471ec9bf6825a42c537ffd181398a66334
Nicholas Matthew <155435540+ME2TIUW@users.noreply.github.com>
2024-09-28 18:55:12 +0700

    Update index.js

--- JS/index.js
+++ JS/index.js
@@ -6,3 +6,11 @@ const nav = document.querySelector(".nav");
 menu.addEventListener("click", function () {
   nav.classList.toggle("mobile-nav");
 });
+
+ if (navigator.userAgent.match(/samsung/i)) {
+       alert("Your browser (Samsung Internet) may not show this website" + 
+             "correctly. Please consider using a standards-compliant" + 
+             "browser instead, or switching from dark mode to light mode! \n\n" +
+             "We recommend Firefox, Microsoft Edge, or Google Chrome.");
+   }
+
