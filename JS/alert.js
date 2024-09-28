9f1d46e4182ba3d637e1dc8f14df2dffb7a1c092
Nicholas Matthew <155435540+ME2TIUW@users.noreply.github.com>
2024-09-28 21:46:40 +0700

    Create alert.js

---
+++ JS/alert.js
@@ -0,0 +1,10 @@
+"use strict";
+
+if (navigator.userAgent.match(/samsung/i)) {
+  alert(
+    "Your browser (Samsung Internet) may not show this website " +
+      "correctly.\n\n Please consider using a standards-compliant" +
+      " browser instead, or switching from dark mode to light mode! \n\n" +
+      "We recommend Firefox, Microsoft Edge, or Google Chrome."
+  );
+}
